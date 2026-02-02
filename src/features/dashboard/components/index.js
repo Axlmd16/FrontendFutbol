1a389fed3c7c0860a618a1e4fcff7be373956577
Axlmd <jostin.jimenez@unl.edu.ec>
2026-02-02 12:34:43 -0500

    Dashboard para adminstrador, entreandor y pasasnte

---
+++ src/features/dashboard/components/index.js
@@ -0,0 +1,7 @@
+/**
+ * Exportación de componentes del Dashboard
+ */
+
+export { default as DashboardCard } from "./DashboardCard";
+export { default as WelcomeHeader } from "./WelcomeHeader";
+export { default as QuickActions } from "./QuickActions";
